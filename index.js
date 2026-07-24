d7899e2a37c6116a9725907d628b91d264c4faf3
victortowers <202715235+victortowers@users.noreply.github.com>
2026-07-24 20:40:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,63 @@
-import TurndownService from 'turndown';
+function htmlToMarkdown(html) {
+  let md = html;
 
-const turndownService = new TurndownService({
-  headingStyle: 'atx',
-  codeBlockStyle: 'fenced',
-  bulletListMarker: '-',
-});
+  // Remove script and style tags
+  md = md.replace(/<script[\s\S]*?<\/script>/gi, '');
+  md = md.replace(/<style[\s\S]*?<\/style>/gi, '');
+
+  // Headers
+  md = md.replace(/<h1[^>]*>([\s\S]*?)<\/h1>/gi, '# $1\n\n');
+  md = md.replace(/<h2[^>]*>([\s\S]*?)<\/h2>/gi, '## $1\n\n');
+  md = md.replace(/<h3[^>]*>([\s\S]*?)<\/h3>/gi, '### $1\n\n');
+
+  // Bold and italic
+  md = md.replace(/<strong>([\s\S]*?)<\/strong>/gi, '**$1**');
+  md = md.replace(/<b>([\s\S]*?)<\/b>/gi, '**$1**');
+  md = md.replace(/<em>([\s\S]*?)<\/em>/gi, '*$1*');
+  md = md.replace(/<i>([\s\S]*?)<\/i>/gi, '*$1*');
+
+  // Links
+  md = md.replace(/<a[^>]*href=["']([^"']*)["'][^>]*>([\s\S]*?)<\/a>/gi, '[$2]($1)');
+
+  // Images
+  md = md.replace(/<img[^>]*src=["']([^"']*)["'][^>]*alt=["']([^"']*)["'][^>]*\/?>/gi, '![$2]($1)');
+  md = md.replace(/<img[^>]*src=["']([^"']*)["'][^>]*\/?>/gi, '![]($1)');
+
+  // Paragraphs
+  md = md.replace(/<p[^>]*>([\s\S]*?)<\/p>/gi, '$1\n\n');
+
+  // Lists
+  md = md.replace(/<li[^>]*>([\s\S]*?)<\/li>/gi, '- $1\n');
+  md = md.replace(/<ul[^>]*>([\s\S]*?)<\/ul>/gi, '$1\n');
+  md = md.replace(/<ol[^>]*>([\s\S]*?)<\/ol>/gi, '$1\n');
+
+  // Code blocks
+  md = md.replace(/<pre><code>([\s\S]*?)<\/code><\/pre>/gi, '```\n$1\n```\n');
+  md = md.replace(/<code>([\s\S]*?)<\/code>/gi, '`$1`');
+
+  // Horizontal rules
+  md = md.replace(/<hr\s*\/?>/gi, '---\n\n');
+
+  // Line breaks
+  md = md.replace(/<br\s*\/?>/gi, '\n');
+
+  // Strip remaining HTML tags
+  md = md.replace(/<[^>]*>/g, '');
+
+  // Decode entities
+  md = md.replace(/&amp;/g, '&');
+  md = md.replace(/&lt;/g, '<');
+  md = md.replace(/&gt;/g, '>');
+  md = md.replace(/&quot;/g, '"');
+  md = md.replace(/&#39;/g, "'");
+  md = md.replace(/&nbsp;/g, ' ');
+
+  // Clean up excessive whitespace
+  md = md.replace(/\n{3,}/g, '\n\n');
+  md = md.replace(/[ \t]+/g, ' ');
+
+  return md.trim();
+}
 
 function estimateTokens(text) {
   return Math.round(text.length / 4);
@@ -16,9 +69,11 @@ export default {
     if (!accept.includes('text/markdown')) {
       return fetch(request);
     }
+
     const response = await fetch(request);
     const html = await response.text();
-    const markdown = turndownService.turndown(html);
+    const markdown = htmlToMarkdown(html);
+
     return new Response(markdown, {
       headers: {
         'Content-Type': 'text/markdown; charset=utf-8',
